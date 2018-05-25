1c521a80e9e7493bcb3fe2dc2c0d0ab570202e49
Mark Heckler <mark.heckler@gmail.com>
2018-05-25 14:34:04 +0200

    Removed pom-based Kotlin project

--- spring-kotlin-angular/src/main/kotlin/com/kotlinexample/springkotlinangular/PaginatedListWrapper.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlinexample.springkotlinangular
-
-class PaginatedListWrapper(
-    val currentPage: Int? = 1,
-    val pageSize: Int? = 10,
-    val totalResults: Int? = 10,
-    val sortFields: String? = "id",
-    val sortDirections: String? = "asc",
-    val list: List<Person>? = null)
--- spring-kotlin-angular/src/main/kotlin/com/kotlinexample/springkotlinangular/PersonRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.kotlinexample.springkotlinangular
-
-import org.springframework.data.repository.PagingAndSortingRepository
-
-interface PersonRepository : PagingAndSortingRepository<Person, Long>
